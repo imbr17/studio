d9866d01a299724988158caf66ab6175cc6d048a
Istiaq Mahmud <istiaqbiplob@gmail.com>
2025-04-18 16:02:53 +0000

    those all pages link include everypage as a button animation style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type {Metadata} from 'next';
 import {Geist, Geist_Mono} from 'next/font/google';
 import './globals.css';
-import {SidebarProvider} from "@/components/ui/sidebar";
+import {SidebarProvider, Sidebar, SidebarContent, SidebarMenu, SidebarMenuItem, SidebarMenuButton} from "@/components/ui/sidebar";
+import Link from "next/link";
+import { Icons } from "@/components/icons";
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,7 +29,63 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <SidebarProvider>
-          {children}
+          <div className="flex">
+            <Sidebar>
+              <SidebarContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <Link href="/" passHref legacyBehavior>
+                      <SidebarMenuButton >
+                        <Icons.home className="mr-2 h-4 w-4" />
+                        <span>Home</span>
+                      </SidebarMenuButton>
+                    </Link>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <Link href="/info" passHref legacyBehavior>
+                      <SidebarMenuButton>
+                        <Icons.help className="mr-2 h-4 w-4" />
+                        <span>Info</span>
+                      </SidebarMenuButton>
+                    </Link>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <Link href="/books" passHref legacyBehavior>
+                      <SidebarMenuButton>
+                        <Icons.bookOpen className="mr-2 h-4 w-4" />
+                        <span>Books</span>
+                      </SidebarMenuButton>
+                    </Link>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <Link href="/librarians" passHref legacyBehavior>
+                      <SidebarMenuButton>
+                        <Icons.user className="mr-2 h-4 w-4" />
+                        <span>Librarian</span>
+                      </SidebarMenuButton>
+                    </Link>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <Link href="/admin/login" passHref legacyBehavior>
+                      <SidebarMenuButton>
+                        <Icons.lock className="mr-2 h-4 w-4" />
+                        <span>Admin Login</span>
+                      </SidebarMenuButton>
+                    </Link>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <Link href="/user/dashboard" passHref legacyBehavior>
+                      <SidebarMenuButton>
+                        <Icons.user className="mr-2 h-4 w-4" />
+                        <span>Member Area</span>
+                      </SidebarMenuButton>
+                    </Link>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarContent>
+            </Sidebar>
+            {children}
+          </div>
         </SidebarProvider>
       </body>
     </html>
